e9767b4e0531dafa67af292d8d03070c5297ca65
saskia.sonnberg <saskiasonnberg@gmail.com>
2019-07-17 15:52:45 +0300

    UserController

---
+++ Uptime.CredentialManager.Web/Models/Credential.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uptime.CredentialManager.Web.Models
+{
+    public class Credential
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Uptime.CredentialManager.Web/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uptime.CredentialManager.Web.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
