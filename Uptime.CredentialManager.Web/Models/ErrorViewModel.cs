9be91fbf0b9830aca475c2eac32ab1d191fa3cf9
saskia.sonnberg <saskiasonnberg@gmail.com>
2019-07-17 15:30:38 +0300

    Add project files.

---
+++ Uptime.CredentialManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Uptime.CredentialManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
